bbdf529e86e2439779db7664f2e8556f8ff5b156
TechlabRS <129305203+TechlabRS@users.noreply.github.com>
2023-08-09 23:33:05 +0530

    Create main.tf

---
+++ zone-op/main.tf
@@ -0,0 +1,16 @@
+data "terraform_remote_state" "cr_zone" {
+  backend = "local"  # Replace with the appropriate backend configuration if using a remote state.
+
+  config = {
+    path = "../cr_zone/terraform.tfstate"  # Adjust the path to the Terraform state file accordingly.
+  }
+}
+
+# You can now access the Route53 zone ID like this:
+output "zn_id"{
+ value = data.terraform_remote_state.cr_zone.outputs.zone_id
+}
+
+output "ky_id"{
+ value = data.terraform_remote_state.cr_zone.outputs.myky
+}
