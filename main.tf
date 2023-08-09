636b2bf175d26d4663738d71e649dfb1369d29a8
TechlabRS <129305203+TechlabRS@users.noreply.github.com>
2023-08-09 23:36:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,1 @@
-resource "aws_route53_zone" "myzone" {
-  name = "uknowme.tech"
-}
-
-resource "aws_kms_key" "mykey" {
-  description             = "my key for kms"
-  deletion_window_in_days = 7
-}
+#locals
